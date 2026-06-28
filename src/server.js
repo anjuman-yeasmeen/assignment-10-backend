28712575bcb29e48f2a0bfb75c0d4475fa401109
anjuman-yeasmeen <malarashid14@gmail.com>
2026-06-28 17:04:46 +0600

    udpated cors setting

--- src/server.js
+++ src/server.js
@@ -25,7 +25,12 @@ const allowedOrigins = (process.env.CLIENT_ORIGINS || "http://localhost:3000")
 // In development, reflect any origin so LAN devices (phones, tablets) can reach
 // the API. In production, restrict to the configured allowlist.
 const isDev = process.env.NODE_ENV !== "production";
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
